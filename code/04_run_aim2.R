8c2974b2404b04fabf471d070ec3ad05d73daf8f
Gus Hamilton <gushamilton@gmail.com>
2025-07-18 08:51:57 +0100

    Complete parameter correction: update remaining files with corrected frequency and mortality data
    Updated all remaining active files with corrected parameters:
    - code/04_run_aim2.R: Updated frequency vector (758 total) and mortality data (overall across both arms)
    - code/functions/aim_3.r: Updated frequency vector and mortality data
    - parallel/debug_test.R: Updated frequency vector and mortality data
    - parallel/debug_enrichment.R: Updated frequency vector and mortality data
    - parallel/debug_detailed.R: Updated frequency vector and mortality data
    
    All active code files now use consistent, corrected parameter values from Swets et al. CID paper:
    - Overall prevalence: 758 total patients (both arms)
    - Overall mortality: A=25/115, B=8/107, C=53/276, D=22/121, E=4/139

--- code/04_run_aim2.R
+++ code/04_run_aim2.R
@@ -40,9 +40,18 @@ scenario_definitions <- tribble(
 )
 
 # --- Shared Parameters ---
-freq_arrest        <- c(A = 60/388, B = 52/388, C = 138/388, D = 69/388, E = 69/388)
-p0_arrest_raw      <- c(A = 7/60, B = 0/52, C = 11/138, D = 11/69, E = 1/69)
-p0_B_corrected     <- 0.5 / (52 + 0.5)
+# Updated to use overall prevalence (both treatment and control arms combined)
+# From Swets et al. CID Supplementary Table 2:
+# Group A: 60+55=115, Group B: 52+55=107, Group C: 138+138=276, Group D: 69+52=121, Group E: 69+70=139
+# Total: 115+107+276+121+139 = 758
+freq_arrest        <- c(A = 115/758, B = 107/758, C = 276/758, D = 121/758, E = 139/758)
+# Updated mortality data based on paper (overall mortality across both arms)
+# From Swets et al. CID Supplementary Table 2:
+# Group A: (13+12)/(60+55)=25/115=21.7%, Group B: (0+8)/(52+55)=8/107=7.5%, 
+# Group C: (29+24)/(138+138)=53/276=19.2%, Group D: (11+11)/(69+52)=22/121=18.2%, 
+# Group E: (3+1)/(69+70)=4/139=2.9%
+p0_arrest_raw      <- c(A = 25/115, B = 8/107, C = 53/276, D = 22/121, E = 4/139)
+p0_B_corrected     <- 0.5 / (107 + 0.5)
 p0_arrest_adjusted <- p0_arrest_raw
 p0_arrest_adjusted["B"] <- p0_B_corrected
 alpha_bonferroni   <- 0.05 / 5
--- code/functions/aim_3.r
+++ code/functions/aim_3.r
@@ -17,10 +17,19 @@ BASE_SEED <- 3000
 
 # --- Population & Effect Parameters (Copied from QMD) ---
 or_mortality       <- c(A = 1.0, B = 18.8, C = 0.79, D = 1.4, E = 0.3)
-freq_arrest        <- c(A = 60/388, B = 52/388, C = 138/388, D = 69/388, E = 69/388)
-p0_arrest_raw      <- c(A = 7/60, B = 0/52, C = 11/138, D = 11/69, E = 1/69)
-n0_B <- 52
-p0_B_corrected <- 0.001
+# Updated to use overall prevalence (both treatment and control arms combined)
+# From Swets et al. CID Supplementary Table 2:
+# Group A: 60+55=115, Group B: 52+55=107, Group C: 138+138=276, Group D: 69+52=121, Group E: 69+70=139
+# Total: 115+107+276+121+139 = 758
+freq_arrest        <- c(A = 115/758, B = 107/758, C = 276/758, D = 121/758, E = 139/758)
+# Updated mortality data based on paper (overall mortality across both arms)
+# From Swets et al. CID Supplementary Table 2:
+# Group A: (13+12)/(60+55)=25/115=21.7%, Group B: (0+8)/(52+55)=8/107=7.5%, 
+# Group C: (29+24)/(138+138)=53/276=19.2%, Group D: (11+11)/(69+52)=22/121=18.2%, 
+# Group E: (3+1)/(69+70)=4/139=2.9%
+p0_arrest_raw      <- c(A = 25/115, B = 8/107, C = 53/276, D = 22/121, E = 4/139)
+n0_B <- 107
+p0_B_corrected <- 0.5 / (n0_B + 0.5)
 p0_arrest_adjusted <- p0_arrest_raw
 p0_arrest_adjusted["B"] <- p0_B_corrected
 
